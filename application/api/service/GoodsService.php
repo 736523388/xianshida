a12673d34cd75abda54634247e7dea172319fb92
forska <736523388@qq.com>
2022-08-18 14:27:54 +0800

    '兼容'

--- application/api/service/GoodsService.php
+++ application/api/service/GoodsService.php
@@ -170,7 +170,7 @@ class GoodsService
             $goods_spec = Db::name('StoreGoodsSpec')->where('id',$goodsdetail['spec_id'])->find();
             $spec_list = json_decode(isset($goods_spec['spec_param']) ? $goods_spec['spec_param'] : '',true);
             foreach ($spec_list as $key => $item) {
-                $spec_list[$key]['value'] = !empty(explode(',',$item['value'])) ? explode(',',$item['value']) : !empty(explode(' ',$item['value'])) ? explode(' ',$item['value']) :[];
+                $spec_list[$key]['value'] = !empty(explode(',',$item['value'])) ? explode(',',$item['value']) : (!empty(explode(' ',$item['value'])) ? explode(' ',$item['value']) :[]);
             }
             $goodsdetail['spec_list'] = $spec_list;
         }else{
