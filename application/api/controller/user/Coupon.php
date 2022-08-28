d8a167e2276ca92cada74c2484378f3b8175fb39
forska <736523388@qq.com>
2022-08-28 17:22:35 +0800

    '优惠券调整'

--- application/api/controller/user/Coupon.php
+++ application/api/controller/user/Coupon.php
@@ -20,13 +20,29 @@ class Coupon extends BasicUserApi
      */
     public function index(){
         $goods_id = $this->request->param('goods_id',0);
-        $coupon_list = CouponService::getAvailableCoupon(UID,$goods_id);
-        foreach ($coupon_list as &$item) {
+        // 排除当前用户已领取且当前时间正有效的优惠券列表
+        $mid = UID;
+        $res = Db::query("select * from store_coupon where status = 1 and is_deleted = 0 and (coupon_stock = 0 or coupon_stock - coupon_sale > 0)
+and NOT EXISTS(select id from store_coupon_user where mid = {$mid} and coupon_id = store_coupon.id and coupon_end_time > unix_timestamp(now()))");
+        foreach ($res as &$item) {
             $item['coupon_quota'] = floatval($item['coupon_quota']);
             $item['coupon_discount'] = floatval($item['coupon_discount']);
             $item['use_threshold'] = floatval($item['use_threshold']);
+            // 有效期处理
+            if($item['time_type'] == 2){
+                $item['coupon_start_time'] = strtotime(date('Y-m-d 00:00:00'));
+                $item['coupon_end_time'] = strtotime(date('Y-m-d 23:59:59', strtotime("+ {$item['coupon_day']} day")));
+            }
         }
-        $this->success('success',$coupon_list);
+        $this->success('success', $res);
+//        dump($res);exit();
+//        $coupon_list = CouponService::getAvailableCoupon(UID,$goods_id);
+//        foreach ($coupon_list as &$item) {
+//            $item['coupon_quota'] = floatval($item['coupon_quota']);
+//            $item['coupon_discount'] = floatval($item['coupon_discount']);
+//            $item['use_threshold'] = floatval($item['use_threshold']);
+//        }
+//        $this->success('success',$coupon_list);
     }
 
     /**
@@ -86,6 +102,63 @@ class Coupon extends BasicUserApi
      */
     public function receive(){
         $id = $this->request->param('id');
+        $mid = UID;
+        $res = Db::query("select * from store_coupon where id = {$id} and status = 1 and is_deleted = 0 and (coupon_stock = 0 or coupon_stock - coupon_sale > 0)
+and NOT EXISTS(select id from store_coupon_user where mid = {$mid} and coupon_id = {$id} and coupon_end_time > unix_timestamp(now()))");
+        if(empty($res)){
+            $this->error("领取优惠券失败");
+        }
+        $coupon = $res[0];
+        $integral = $coupon['integral'];
+        if($integral > 0){
+            //如果积分不够
+            $member_integral = Db::name('StoreMember')->where(['id' => $mid])->value('integral');
+            if($member_integral < $integral){
+                $this->error("您的积分不够，兑换失败");
+            }
+        }
+        $data = [
+            'mid' => $mid,
+            'coupon_id' => $id,
+            'coupon_name' => $coupon['coupon_name'],
+            'coupon_quota' => $coupon['coupon_quota'],
+            'use_threshold' => $coupon['use_threshold'],
+            'coupon_auth_type' => $coupon['coupon_auth_type']
+        ];
+        if($coupon['time_type'] == 1){
+            $data['coupon_start_time'] = $coupon['coupon_start_time'];
+            $data['coupon_end_time'] = $coupon['coupon_end_time'];
+        }elseif ($coupon['time_type'] == 2){
+            $data['coupon_start_time'] = strtotime(date('Y-m-d 00:00:00'));
+            $data['coupon_end_time'] = strtotime(date('Y-m-d 23:59:59', strtotime("+ {$coupon['coupon_day']} day")));
+        }
+        if($data['coupon_auth_type'] == 2){
+            $data['coupon_auth_cate'] = $coupon['coupon_auth_cate'];
+        }elseif ($data['coupon_auth_type'] == 3){
+            $data['coupon_auth_brand'] = $coupon['coupon_auth_brand'];
+        }elseif ($data['coupon_auth_type'] == 4){
+            $data['coupon_auth_goods'] = $coupon['coupon_auth_goods'];
+        }
+        try{
+            Db::transaction(function () use ($data, $id, $mid, $integral) {
+                Db::table('store_coupon_user')->insert($data);
+                Db::table('store_coupon')->where('id',$id)->setInc('coupon_sale',1);
+                if($integral > 0){
+                    Db::name('StoreMember')->where(['id' => $mid])->setDec('integral', $integral);
+                    Db::table('store_member_integral_log')->insert([
+                        'mid' => $mid,
+                        'integral' => -$integral,
+                        'desc' => "兑换优惠券【{$data['coupon_name']}】花费{$integral}积分"
+                    ]);
+                }
+            });
+        } catch (HttpResponseException $exception) {
+            return $exception->getResponse();
+        } catch (\Exception $e) {
+            $this->error('领取优惠券失败,' . $e->getMessage());
+        }
+        $this->success('领取优惠券成功');
+        dump($res);exit();
         try{
             CouponService::ReceiveCoupon(UID,$id);
         } catch (HttpResponseException $exception) {
--- application/store/controller/Coupon.php
+++ application/store/controller/Coupon.php
@@ -112,27 +112,20 @@ class Coupon extends BasicAdmin
     protected function _form_filter(&$data)
     {
         if ($this->request->isGet()) {
-            $data['use_level'] = explode(',', isset($data['use_level']) ? $data['use_level'] : '');
+//            $data['use_level'] = explode(',', isset($data['use_level']) ? $data['use_level'] : '');
+            // 使用时间类型处理
             if(isset($data['time_type']) && $data['time_type'] == 1){
-                $data['coupon_time']=  date('Y-m-d',$data['coupon_start_time']). ' - ' .date('Y-m-d',$data['coupon_end_time']);
+                $data['coupon_time'] =  date('Y-m-d',$data['coupon_start_time']). ' - ' .date('Y-m-d',$data['coupon_end_time']);
             }else{
-                $data['coupon_time']=  '';
+                $data['coupon_time'] =  '';
             }
-
+            // 数据绑定
             $this->_form_assign();
         }else{
             if(!isset($data['level_limits'])) $data['level_limits'] = 0;
-            if(!isset($data['time_type'])) $data['time_type'] = 1;
-
-            if($data['level_limits']=='1'){
-                if (isset($data['use_level']) && is_array($data['use_level'])) {
-                    sort($data['use_level']);
-                    $data['use_level'] = join(',', $data['use_level']);
-                }else{
-                    $this->error('请勾选会员等级');
-                }
-            }
-            if($data['time_type'] == '1'){
+            if(!isset($data['time_type'])) $data['time_type'] = 2;
+            if(empty($data['coupon_day'])) $this->error("请输入有效期天数");
+            if($data['time_type'] == 1){
                 if (isset($data['coupon_time']) && $data['coupon_time'] !== '') {
                     list($start, $end) = explode(' - ', $data['coupon_time']);
                     $data['coupon_start_time'] = strtotime("{$start} 00:00:00");
@@ -142,20 +135,18 @@ class Coupon extends BasicAdmin
                     $this->error('请选择日期范围');
                 }
             }
-            if($data['coupon_auth_type'] == '2'){
-                if(!isset($data['coupon_auth_cate']) || $data['coupon_auth_cate'] == ''){
-                    $this->error('请选择商品分类');
-                }
-            }elseif ($data['coupon_auth_type'] == '3'){
-                if(!isset($data['coupon_auth_brand']) || $data['coupon_auth_brand'] == ''){
-                    $this->error('请选择商品品牌');
-                }
-            }elseif ($data['coupon_auth_type'] == '4'){
-                if(!isset($data['coupon_auth_goods']) || $data['coupon_auth_goods'] == ''){
-                    $this->error('请选择商品');
+            $coupon_auth_type_arr = [
+                2 => ['key' => 'coupon_auth_cate', 'title' => '商品分类', 'type' => 'change'],
+                3 => ['key' => 'coupon_auth_brand', 'title' => '商品品牌', 'type' => 'change'],
+                4 => ['key' => 'coupon_auth_goods', 'title' => '商品', 'type' => 'change'],
+            ];
+            if(in_array(intval($data['coupon_auth_type']), [2,3,4])){
+                $auth_key = $coupon_auth_type_arr[intval($data['coupon_auth_type'])]['key'];
+                $auth_title = $coupon_auth_type_arr[intval($data['coupon_auth_type'])]['title'];
+                if(!isset($data[$auth_key]) || empty($data[$auth_key])){
+                    $this->error("请选择{$auth_title}");
                 }
             }
-
             //dump($data);exit();
         }
 
@@ -172,14 +163,12 @@ class Coupon extends BasicAdmin
      */
     protected function _form_assign(){
         list($where, $order) = [['status' => '1', 'is_deleted' => '0'], 'sort asc,id desc'];
-        $levels = (array)Db::table('store_member_level')->where($where)->order($order)->select();
         $brands = (array)Db::name('StoreGoodsBrand')->where($where)->order($order)->select();
         $cates = (array)Db::name('StoreGoodsCate')->where($where)->order($order)->select();
         $goods = (array)Db::name('StoreGoods')->field('id,goods_title')->where($where)->select();
         $this->assign([
             'cates'  => ToolsService::arr2table($cates),
             'brands' => $brands,
-            'levels' => $levels,
             'goods' => $goods
         ]);
     }
